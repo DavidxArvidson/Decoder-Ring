16d266845fffad5eaa9fb29a42b62d34c9102ae1
David Arvidson <arvidson6@gmail.com>
2021-10-29 15:49:28 -0400

    First Attempt

--- src/caesar.js
+++ src/caesar.js
@@ -5,11 +5,30 @@
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
-
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
   function caesar(input, shift, encode = true) {
     // your solution code here
+    let result = "";
+    if (!shift || shift === 0 || shift < -25 || shift > 25) return false;
+    if (encode === false) shift *= -1;
+    for (let i = 0; i < input.length; i++) {
+      // if string character is a letter
+      if (alphabet.includes(input[i].toLowerCase)) {
+        let alphaIndex = (function () {
+          for (let j = 0; j < alphabet.length; j++) {
+            if (alphabet[j] === input[i].toLowerCase) return j;
+          }
+        });
+        let letterIndex = alphaIndex + shift;
+        if (letterIndex > 25) letterIndex -= 25;
+        if (letterIndex < 0) letterIndex += 25;
+        let letter = alphabet[letterIndex];
+        result += letter;
+      }
+      else result += input[i];
+    }
+    return result;
   }
-
   return {
     caesar,
   };
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,27 @@
 // Write your tests here!
+const caesar = require("../src/caesar");
+const expect = require("chai").expect;
+
+describe("caesar", () => {
+    it("should return false if the shift value is greater than 25", () => {
+        const expected = false;
+        const actual = caesar("test", 30);
+        expect(actual).to.equal(expected);
+    });
+});
+
+describe("caesar", () => {
+    it("should return the input encoded properly", () => {
+        const expected = "bpqa qa i amkzmb umaaiom!";
+        const actual = caesar("This is a secret message!", 8);
+        expect(actual).to.equal(expected);
+    });
+});
+
+describe("caesar", () => {
+    it("should return the input decoded properly", () => {
+        const expected = "This is a secret message!";
+        const actual = caesar("bpqa qa i amkzmb umaaiom!", 8, false);
+        expect(actual).to.equal(expected);
+    });
+});

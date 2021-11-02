c1ab455cf323e0ffb1030f7ee198dc6e34d8d5dc
David Arvidson <arvidson6@gmail.com>
2021-11-02 17:27:35 -0400

    Substitution Final

--- src/substitution.js
+++ src/substitution.js
@@ -5,9 +5,51 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
-
+  const actual = "abcdefghijklmnopqrstuvwxyz";
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+    if (!alphabet) return false;
+    // if any characters in "alphabet" repeat, return false
+    for (let i = 0; i < alphabet.length; i++) {
+      if (alphabet.indexOf(alphabet[i]) !== alphabet.lastIndexOf(alphabet[i])) return false;
+    }
+    // if alphabet isn't exactly 26 characters, return false
+    if (alphabet.length !== 26) return false;
+    // encode
+    if (encode) {
+      let result = "";
+      input = input.toLowerCase();
+      for (let j = 0; j < input.length; j++) {
+        if (input[j] === " ") {
+          result += input[j];
+        } else {
+          let alphaIndex = (() => {
+            for (let k = 0; k < actual.length; k++) {
+              if (input[j] === actual[k]) return k;
+            }
+          })
+          result += alphabet[alphaIndex()];
+        }
+      }
+      return result;
+    // decode
+    } else {
+      let result = "";
+      input = input.toLowerCase();
+      for (let j = 0; j < input.length; j++) {
+        if (input[j] === " ") {
+          result += input[j];
+        } else {
+          let alphaIndex = (() => {
+            for (let k = 0; k < alphabet.length; k++) {
+              if (input[j] === alphabet[k]) return k;
+            }
+          })
+          result += actual[alphaIndex()];
+        }
+      }
+      return result;
+    } 
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,35 @@
 // Write your tests here!
+const {substitution} = require("../src/substitution");
+const expect = require("chai").expect;
+
+describe("substitution", () => {
+    it("should return false if 'alphabet' is not provided", () => {
+        const expected = false;
+        const actual = substitution("message");
+        expect(actual).to.equal(expected);
+    });
+
+    it("should return false if any characters in 'alphabet' repeat", () => {
+        const expected = false;
+        const actual = substitution("message", "abcabcabcabcabcabcabcabcyz");
+        expect(actual).to.equal(expected);
+    });
+
+    it("should return false if 'alphabet' isn't exactly 26 characters", () => {
+        const expected = false;
+        const actual = substitution("message", "acdbefghijklmnopqrstuvwxzy#@$");
+        expect(actual).to.equal(expected);
+    });
+
+    it("should return the input encoded properly", () => {
+        const expected = "jrufscpw";
+        const actual = substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev");
+        expect(actual).to.equal(expected);
+    });
+
+    it("should return the input decoded properly", () => {
+        const expected = "message";
+        const actual = substitution("y&ii$r&", "$wae&zrdxtfcygvuhbijnokmpl", false);
+        expect(actual).to.equal(expected);
+    });
+});
